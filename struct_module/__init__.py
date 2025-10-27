125ad42a64e663610f837efb134c1a82fa598f31
Kenneth Belitzky <kenny@belitzky.com>
2025-10-27 11:28:34 -0300

    wip

--- struct_module/__init__.py
+++ struct_module/__init__.py
@@ -1,6 +1,6 @@
 from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = version(__name__)
+    __version__ = version('struct')
 except PackageNotFoundError:
     __version__ = "unknown"
